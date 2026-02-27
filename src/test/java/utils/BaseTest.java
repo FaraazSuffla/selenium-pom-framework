99c5e48607ca2e3608525479fdc7af6be7d6cc70
Ferris <110396388+FaraazSuffla@users.noreply.github.com>
2026-02-27 17:52:41 +0200

    Fix: Add @Optional default, alwaysRun=true and system property override in BaseTest

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -3,22 +3,24 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     @Parameters({"browser"})
-    public void setUp(String browser) {
-        DriverManager.initDriver(browser != null ? browser : System.getProperty("browser", "chrome"));
+    public void setUp(@Optional("chrome") String browser) {
+        String browserToUse = System.getProperty("browser", browser);
+        DriverManager.initDriver(browserToUse);
         driver = DriverManager.getDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com");
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         DriverManager.quitDriver();
     }
